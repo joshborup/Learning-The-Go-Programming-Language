39366d8d14ed0cdd517b19449904a7c3a9a43d29
josh <joshborup@gmail.com>
2019-06-28 01:34:24 -0700

    commiting for the night

--- if.go
+++ if.go
@@ -5,5 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println("ifs")
+	a := 10
+	b := 20
+	c := 30
+
+	// go doesnt need parenthesis for if statements
+	if a < b {
+		fmt.Println(a, "is greater than", b)
+	}
+	// go allows if else statememt for logic flow control
+	if b < a {
+		fmt.Println(b, "is greater than", a)
+	} else if c > b {
+		fmt.Println(c, "is greater than", b)
+	}
+	// go allows you to assign variables inline with the if statment that is testing the variables value
+	if fraction := b / a; fraction < 3 {
+		fmt.Println(fraction, "is less than", 3)
+	}
 }
